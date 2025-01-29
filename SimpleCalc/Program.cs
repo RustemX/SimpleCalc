cac156921fc50aa1143c2de927d4990f4f533f18
= <=>
2025-01-29 15:10:52 +0500

    Refactor program

--- SimpleCalc/Program.cs
+++ SimpleCalc/Program.cs
@@ -1,29 +1,54 @@
-﻿while(true){
-Console.Write("Введите первое число:");
-double a = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите второе число:");
-double b = Convert.ToDouble(Console.ReadLine());
-Console.Write("Выберите операцию (+ - * /):");
-string mathOperator = Console.ReadLine()!;
-if (mathOperator.Equals("+"))
-{
-    Console.WriteLine(a + b);
-}
-if (mathOperator.Equals("-"))
-{
-    Console.WriteLine(a - b);
-}
-if(mathOperator.Equals("*"))
-{
-    Console.WriteLine(a * b);
-}
-if(mathOperator.Equals("/"))
-{
-    if(b == 0)
-    {
-        Console.WriteLine("На ноль делить нельзя!");
-    }
-    else{Console.WriteLine(a / b);}
-    
-}
-}
+﻿while (true) 
+        {
+            // Ввод первого числа с проверкой
+            double num1;
+            Console.Write("Введите первое число (или 'exit' для выхода): ");
+            string input = Console.ReadLine()!;
+            if (input.ToLower() == "exit") break;
+            if (!double.TryParse(input, out num1)) 
+            {
+                Console.WriteLine("Ошибка: введите число!");
+                continue;
+            }
+
+            // Ввод второго числа с проверкой
+            double num2;
+            Console.Write("Введите второе число: ");
+            if (!double.TryParse(Console.ReadLine(), out num2)) 
+            {
+                Console.WriteLine("Ошибка: введите число!");
+                continue;
+            }
+
+            // Выбор операции
+            Console.Write("Выберите операцию (+ - * /): ");
+            string? mathOperator = Console.ReadLine();
+
+            switch (mathOperator) 
+            {
+                case "+":
+                    Console.WriteLine($"Результат: {num1 + num2}");
+                    break;
+                case "-":
+                    Console.WriteLine($"Результат: {num1 - num2}");
+                    break;
+                case "*":
+                    Console.WriteLine($"Результат: {num1 * num2}");
+                    break;
+                case "/":
+                    if (num2 == 0) 
+                    {
+                        Console.WriteLine("Ошибка: на ноль делить нельзя!");
+                    } 
+                    else 
+                    {
+                        Console.WriteLine($"Результат: {num1 / num2}");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Неизвестная операция!");
+                    break;
+            }
+
+            Console.WriteLine(); // Пустая строка для разделения итераций
+        }
