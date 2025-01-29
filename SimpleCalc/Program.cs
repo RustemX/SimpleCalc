40220ecb351d0e69c0ad27f7de9f15db394c6ebd
= <=>
2025-01-29 13:22:15 +0500

    add main method

--- SimpleCalc/Program.cs
+++ SimpleCalc/Program.cs
@@ -1,3 +1,29 @@
-﻿double a = Convert.ToDouble(Console.ReadLine());
+﻿while(true){
+Console.Write("Введите первое число:");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число:");
 double b = Convert.ToDouble(Console.ReadLine());
-string oper = Console.ReadLine();
+Console.Write("Выберите операцию (+ - * /):");
+string mathOperator = Console.ReadLine()!;
+if (mathOperator.Equals("+"))
+{
+    Console.WriteLine(a + b);
+}
+if (mathOperator.Equals("-"))
+{
+    Console.WriteLine(a - b);
+}
+if(mathOperator.Equals("*"))
+{
+    Console.WriteLine(a * b);
+}
+if(mathOperator.Equals("/"))
+{
+    if(b == 0)
+    {
+        Console.WriteLine("На ноль делить нельзя!");
+    }
+    else{Console.WriteLine(a / b);}
+    
+}
+}
